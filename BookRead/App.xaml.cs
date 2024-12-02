4fb8543a953d946c592f2b2414dcba6d51ca8298
DebugDynasty <zilli7733@gmail.com>
2024-12-02 20:04:44 +0200

    Add project files.

---
+++ BookRead/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BookRead
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
