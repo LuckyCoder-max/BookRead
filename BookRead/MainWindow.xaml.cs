085e9cd650c1c5a7813c2e488f5c75bc01748430
DebugDynasty <zilli7733@gmail.com>
2024-12-15 09:36:29 +0200

    Added pdf and epub

--- BookRead/MainWindow.xaml.cs
+++ BookRead/MainWindow.xaml.cs
@@ -1,24 +1,76 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Windows.Forms.Integration;
+using PdfiumViewer;
+using VersOne.Epub;
+using PdfSharp.Pdf.Filters;
 
 namespace BookRead
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private PdfViewer _pdfViewer;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _pdfViewer = new PdfViewer
+            {
+                Dock = System.Windows.Forms.DockStyle.Fill
+            };
+
+            PdfHost.Child = _pdfViewer;
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "PDF Files|*.pdf|EPUB Files|*.epub",
+                Title = "Выберите PDF, Epub файл"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string pdfFilePath = openFileDialog.FileName;
+
+                try
+                {
+
+                    var document = PdfDocument.Load(pdfFilePath);
+                    _pdfViewer.Document?.Dispose();
+                    _pdfViewer.Document = document;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при открытии PDF: {ex.Message}");
+                }
+
+                string epubFilePath = openFileDialog.FileName;
+
+                try
+                {
+                    EpubBook book = EpubReader.ReadBook(epubFilePath);
+
+                    var firstChapter = book.ReadingOrder.FirstOrDefault();
+
+                    if (firstChapter != null)
+                    {
+                        myTextBox.Text = firstChapter.Content;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Книга не содержит доступных глав.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при открытии EPUB: {ex.Message}");
+                }
+            }
+
         }
     }
 }
